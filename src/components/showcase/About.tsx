7fee4ab36d487a7d1177302422fbe3ae5e8b9ff2
getAxle <saihaj@getaxle.io>
2024-12-06 16:50:36 -0500

    Update About.tsx

--- src/components/showcase/About.tsx
+++ src/components/showcase/About.tsx
@@ -12,13 +12,12 @@ const About: React.FC<AboutProps> = (props) => {
         <div className="site-page-content">
             {/* <img src={me} style={styles.topImage} alt="" /> */}
             <h1 style={{ marginLeft: -16 }}>Welcome</h1>
-            <h3>I'm Henry Heffernan</h3>
+            <h3>I'm Saihaj Law</h3>
             <br />
             <div className="text-block">
                 <p>
-                    I'm a software engineer currently working at Vercel! In May
-                    of 2022 I graduated from Rensselaer Polytechnic Institute
-                    with my BS in Computer Science.
+                    I'm a software engineer currently working at a Stealth Startup! In May
+                    of 2024 I entered Western University for Computer Science, and the Ivey School of business for Business Administration!.
                 </p>
                 <br />
                 <p>
@@ -27,8 +26,8 @@ const About: React.FC<AboutProps> = (props) => {
                     building it. If you have any questions or comments, feel
                     free to contact me using{' '}
                     <Link to="/contact">this form</Link> or shoot me an email at{' '}
-                    <a href="mailto:henryheffernan@gmail.com">
-                        henryheffernan@gmail.com
+                    <a href="mailto:saihajlaw@gmail.com">
+                        saihajlaw@gmail.com
                     </a>
                 </p>
             </div>
